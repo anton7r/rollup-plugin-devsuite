999939f8c5d7944b41788c6cb79661475750dada
anton7r <anton.rosnell@elisanet.fi>
2023-07-15 13:13:13 +0300

    refactor the codebase

--- client/client.js
+++ client/client.js
@@ -1,75 +1,70 @@
-/* 
+/*
  *   devsuite
  *   client.js
  */
 
-(function() {
-    //only one devsuite should be running per browsing session
-    if(window.__devsuite__) return;
+(function () {
+  //only one devsuite should be running per browsing session
+  if (window.__devsuite__) return;
+  window.__devsuite__ = true;
 
-    function normalizeUrl(url) {
-        return url.replace(location.origin + "/", "").replace(/\?.*$/, "");
+  const normalizeUrl = (url) =>
+    url.replace(location.origin + "/", "").replace(/\?.*$/, "");
+
+  const port = "%port%",
+    host = "%host%";
+
+  function onMessage(e) {
+    const { data } = e;
+    if (data === "") {
+      return location.reload();
     }
 
-    var port = "%port%";
-    var host = "%host%";
-    
-    function onMessage(e) {
-        const data = e.data;
-        if (data === "") {
-            return location.reload();
-        }
-        var update = JSON.parse(data);
-        update.data = update.data;
-        var wasFound = false;
+    var update = JSON.parse(data);
+    var wasFound = false;
 
-        switch (update.type) {
-            case "css":
+    switch (update.type) {
+      case "css":
+        for (let i = 0; i < document.head.children.length; i++) {
+          const el = document.head.children[i];
 
-                function cssSuccess(el) {
-                    wasFound = true;
-                    const styles = document.createElement("style");
-                    styles.setAttribute("_url", update.filename);
-                    styles.innerHTML = update.data
-                    el.parentElement.insertAdjacentElement("beforeend", styles);
-                    el.parentElement.removeChild(el);
-                }
-                for (let i = 0; i < document.head.children.length; i++) {
-                    const el = document.head.children[i];
-
-                    if (el.localName === "link" && el.rel === "stylesheet") {
-                        if (normalizeUrl(el.href) === update.filename) {
-                            cssSuccess(el);
-                            break;
-                        }
-                    } else if (el.localName === "style" && el.getAttribute("_url") === update.filename) {
-                        cssSuccess(el);
-                        break;
-                    }
-                }
-                
-                break;
-            default:
-                throw new Error(update.type + "is a unhandled case")
+          if (
+            (el.localName === "link" &&
+              el.rel === "stylesheet" &&
+              normalizeUrl(el.href) === update.filename) ||
+            (el.localName === "style" &&
+              el.getAttribute("_url") === update.filename)
+          ) {
+            wasFound = true;
+            const styles = document.createElement("style");
+            styles.setAttribute("_url", update.filename);
+            styles.innerHTML = update.data;
+            el.parentElement.insertAdjacentElement("beforeend", styles);
+            el.parentElement.removeChild(el);
+            break;
+          }
         }
 
-        if (!wasFound) location.reload();
+        break;
+      default:
+        throw new Error(update.type + "is a unhandled case");
     }
-    
-    function onClose(params) {
-        setTimeout(connect, 1000);
-    }
-    
-    function connect() {
-        var ws = new WebSocket("ws://" + host + ":" + port);
-        ws.onmessage = onMessage;
-        ws.onclose = onClose;
-    }
-    
-    try {
-        connect();
-    } catch (ignore) {}
 
-    window.__devsuite__ = true;
+    if (!wasFound) location.reload();
+  }
+
+  function onClose(params) {
+    setTimeout(connect, 1000);
+  }
+
+  function connect() {
+    var ws = new WebSocket("ws://" + host + ":" + port);
+    ws.onmessage = onMessage;
+    ws.onclose = onClose;
+  }
+
+  try {
+    connect();
+  } catch (ignore) {}
 })();
 /* end of client.js */
--- server.js
+++ server.js
@@ -4,159 +4,143 @@ const path = require("path");
 const crypto = require("crypto");
 const http = require("http");
 
-var virtualdir;
-var send;
+let proxies = {};
 
-module.exports.start = function (opts) {
+const server = http.createServer((req, res) => {
+  const baseUrl = req.url.replace("/", "");
 
-    //formalize the keys of proxies
-    var proxies = {};
-    for(key in opts.proxy) {
-        var newKey = key;
-        if(key.startsWith("/")) {
-            newKey = key.replace("/", "")
-        }
-        proxies[newKey] = opts.proxy[key];
-    }
+  for (let proxy in proxies) {
+    if (!baseUrl.startsWith(proxy)) return;
 
-    function getAllFiles(dirname) {
-        var dir = {};
-        fs.readdirSync(dirname).forEach(uri => {
-            if (fs.statSync(path.join(dirname, uri)).isFile()) {
-                dir[uri] = fs.readFileSync(path.join(dirname, uri));
-            } else {
-                var subdir = getAllFiles(path.join(dirname, uri));
-                for (key in subdir) {
-                    dir[uri+"/"+key] = subdir[key];
-                }
-            }
-        })
+    let h = proxies[proxy].split("/");
+    const path = h.length > 1 ? "/" + h[1] : "/";
+    const split = h[0].split(":");
 
-        return dir;
-    }
+    const options = {
+      host: split[0],
+      port: split.length === 1 ? 80 : Number(split[1]),
+      method: req.method,
+      path: (path + baseUrl.replace(proxy, "")).replace(/\/\//g, "/"),
+      headers: req.headers,
+    };
 
-    virtualdir = getAllFiles(opts.dir);
-
-    var server = http.createServer((req, res) => {
-        const baseUrl = req.url.replace("/", "");
-
-        for(proxy in proxies) {
-            if(baseUrl.startsWith(proxy)) {
-                
-                var h = proxies[proxy].split("/");
-                if(!h[1]) {
-                    h[1] = "/"
-                } else {
-                    h[1] = "/" + h[1];
-                }
-
-                var split = h[0].split(":");
-                if(split.length === 1) {
-                    split[1] = 80;
-                } else {
-                    split[1] = Number(split[1]);
-                }
-
-                const options = {
-                    host: split[0],
-                    port: split[1],
-                    method: req.method,
-                    path: (h[1] + baseUrl.replace(proxy, "")).replace(/\/\//g, "/"),
-                    headers: req.headers
-                }
-
-                var proxyReq = http.request(options, function (r) {
-                    res.writeHead(r.statusCode, r.headers)
-                    r.pipe(res, {end: true})
-                })
-
-                req.pipe(proxyReq, {end: true})
-
-                return;
-            }
-        }
-
-        const url = baseUrl.replace(/\[#?].*$/, "");
-        var type = req.headers.accept.split(",")[0];
-
-        if (path.extname(url) === ".js") {
-            type = "application/javascript";
-        }
-
-        res.writeHead(200, {
-            'Content-type': type
-        });
-
-        if (virtualdir[url]) {
-            res.end(virtualdir[url]);
-        } else {
-            var indexFile = virtualdir[opts.index]
-
-            if (indexFile) {
-                res.end(indexFile);
-            } else {
-                res.end(getError("indexNotFound"))
-            }
-        }
+    const proxyReq = http.request(options, (r) => {
+      res.writeHead(r.statusCode, r.headers);
+      r.pipe(res, { end: true });
     });
 
-    const w = new WebSocket.Server({
-        server
-    });
+    req.pipe(proxyReq, { end: true });
 
-    send = data => {
-        w.clients.forEach(c => c.send(data ? JSON.stringify(data) : ""))
-    }
+    return;
+  }
 
-    server.listen(opts.port);
+  const url = baseUrl.replace(/\[#?].*$/, "");
+
+  const type =
+    path.extname(url) === ".js"
+      ? "application/javascript"
+      : req.headers.accept.split(",")[0];
+
+  res.writeHead(200, {
+    "Content-type": type,
+  });
+
+  if (!virtualdir[url]) {
+    const indexFile = virtualdir[opts.index];
+    res.end(indexFile ? indexFile : getError("indexNotFound"));
+    return;
+  }
+
+  res.end(virtualdir[url]);
+});
+
+const w = new WebSocket.Server({ server });
+
+let virtualdir;
+const send = (data) => {
+  w.clients.forEach((c) => c.send(data ? JSON.stringify(data) : ""));
 };
 
-var updated = {};
+function getAllFiles(dirname) {
+  let dir = {};
+
+  fs.readdirSync(dirname).forEach((uri) => {
+    const filePath = path.join(dirname, uri);
+
+    if (!fs.statSync(filePath).isFile()) {
+      for (let key in getAllFiles(filePath)) {
+        dir[uri + "/" + key] = subdir[key];
+      }
+    }
+
+    dir[uri] = fs.readFileSync(filePath);
+  });
+
+  return dir;
+}
+
+module.exports.start = function (opts) {
+  //formalize the keys of proxies
+  for (let key in opts.proxy) {
+    const newKey = key.startsWith("/") ? key.replace("/", "") : key;
+    proxies[newKey] = opts.proxy[key];
+  }
+
+  virtualdir = getAllFiles(opts.dir);
+
+  server.listen(opts.port);
+};
+
+let updated = {};
 
 function genHash(file) {
-    return crypto.createHash("md5").update(file).digest("hex");
+  return crypto.createHash("md5").update(file).digest("hex");
 }
 
 module.exports.update = (filename, content) => {
-    const ext = path.extname(filename).replace(".", "");
+  const ext = path.extname(filename).replace(".", "");
 
-    const hash = genHash(content);
-    if (updated[filename] === hash) return;
-    updated[filename] = hash;
+  const hash = genHash(content);
+  if (updated[filename] === hash) return;
+  updated[filename] = hash;
 
-    if (!virtualdir[filename] || virtualdir[filename] !== content) {
-        //the file is either new or it has updated
-        virtualdir[filename] = content;
-        
-        switch (ext) {
-            case "css":
-                send({
-                    type: ext,
-                    filename,
-                    data: content.toString()
-                });
-                break;
-            
-            default:
-                send();
-                break;
-        }
-    }
-}
+  if (virtualdir[filename] && virtualdir[filename] === content) return;
 
-var errorpages = {};
-var errorcss;
+  //the file is either new or it has updated
+  virtualdir[filename] = content;
 
-const getError = errorFile => {
-    var page = errorpages[errorFile]
-    if (page) {
-        return page;
-    } else {
-        if (!errorcss) {
-            errorcss = "<style>" + fs.readFileSync(require.resolve("./errors/errors.css"), "utf-8").replace(/[ \n\r]/g, "") + "</style>";
-        }
+  switch (ext) {
+    case "css":
+      send({
+        type: ext,
+        filename,
+        data: content.toString(),
+      });
+      break;
 
-        errorpages[errorFile] = fs.readFileSync(require.resolve("./errors/" + errorFile + ".html"), "utf-8").replace("%css%", errorcss);
-        return errorpages[errorFile];
-    }
-}
+    default:
+      send();
+      break;
+  }
+};
+
+let errorpages = {};
+let errorcss;
+const getErrorCss = () => {
+  if (!errorcss) {
+    errorcss = `<style>${fs
+      .readFileSync(require.resolve("./errors/errors.css"), "utf-8")
+      .replace(/[ \n\r]/g, "")}</style>`;
+  }
+  return errorcss;
+};
+
+const getError = (errorFile) => {
+  if (!page) {
+    errorpages[errorFile] = fs
+      .readFileSync(require.resolve("./errors/" + errorFile + ".html"), "utf-8")
+      .replace("%css%", getErrorCss());
+  }
+
+  return errorpages[errorFile];
+};
